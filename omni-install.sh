332d2b1075c2e75362040424bc91f50512429571
ainestal <ainestal@gmail.com>
2015-01-26 22:39:22 +0100

    Update omni-install.sh

--- omni-install.sh
+++ omni-install.sh
@@ -2,3 +2,12 @@
 
 apt-get update
 apt-get install nmap htop apache2 mongodb python-virtualenv vim
+
+#to install rpm on debian
+apt-get install alien
+
+#apache config
+a2enmod proxy
+
+#configure dynamic ip
+apt-get install ddclient
