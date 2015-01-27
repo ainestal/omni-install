d21ca3943ed58550ef528949bddd1a321eeee32d
Alejandro Inestal <ainestal@gmail.com>
2015-01-27 05:05:09 +0100

    add ddclient as standalone option

--- omni.sh
+++ omni.sh
@@ -2,31 +2,24 @@
 
 MY_PATH=$(dirname "${0}")
 
-function main_stack(){
-    echo "INFO - Installing main stack: apache2, mongodb, ddclient"
-    . ${MY_PATH}/lib/main-stack.install
-    echo "INFO - Done !!!"
-}
-
-function lxc(){
-    . ${MY_PATH}/lib/lxc.install
-    echo "INFO - Done !!!"
-}
-
 apt-get install dialog
 cmd=(dialog --separate-output --checklist "Select options:" 10 76 10)
-options=(1 "Install main stack for a standalone server" off    # any option can be set to default to "on"
-         2 "LXC install and create container" off)
+options=(1 "Main Stack - Install for a standalone server" off    # any option can be set to default to "on"
+         2 "LXC        - Install and create container" off
+         3 "DDclient   - Install and configure your dynamic ip" off)
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
 for choice in $choices
 do
     case $choice in
         1)
-            main_stack
+            . ${MY_PATH}/lib/main-stack.install
             ;;
         2)
-            lxc
+            . ${MY_PATH}/lib/lxc.install
+            ;;
+        3)
+            . ${MY_PATH}/lib/ddclient.install
             ;;
     esac
 done
