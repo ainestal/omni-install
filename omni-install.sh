51f6574908bbe7ac33bd6f659a42868b0bf0ec86
ainestal <ainestal@gmail.com>
2015-01-27 04:24:15 +0100

    Update omni-install.sh

--- omni-install.sh
+++ omni-install.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-apt-get update
-apt-get install nmap htop apache2 mongodb python-virtualenv vim
+function main_stack(){
+    echo "INFO - Installing main stack: apache2, mongodb, ddclient"
+    . main-stack.install
+    echo "INFO - Done !!!"
+}
 
-#to install rpm on debian
-apt-get install alien
+function lxc(){
+    echo "INFO - Installing LXC and configuring"
+    . lxc.install
+    echo "INFO - Done !!!"
+}
 
-#apache config
-a2enmod proxy
 
-#configure dynamic ip
-apt-get install ddclient
-bash ddclient.install
+cmd=(dialog --separate-output --checklist "Select options:" 10 76 10)
+options=(1 "Install main stack for a standalone server" off    # any option can be set to default to "on"
+         2 "LXC install and create container" off)
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $choices
+do
+    case $choice in
+        1)
+            main_stack
+            ;;
+        2)
+            lxc
+            ;;
+    esac
+done
