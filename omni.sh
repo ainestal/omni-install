1cbefa385b80852ac061c909050be57457eb4adb
Alejandro Inestal <ainestal@gmail.com>
2015-01-28 04:33:57 +0100

    add docker installation

--- omni.sh
+++ omni.sh
@@ -3,10 +3,11 @@
 MY_PATH=$(dirname "${0}")
 
 apt-get install dialog
-cmd=(dialog --separate-output --checklist "Select options:" 10 76 10)
+cmd=(dialog --separate-output --checklist "Select options:" 12 76 10)
 options=(1 "Main Stack - Install for a standalone server" off    # any option can be set to default to "on"
          2 "LXC        - Install and create container" off
-         3 "DDclient   - Install and configure your dynamic ip" off)
+         3 "DDclient   - Install and configure your dynamic ip" off
+         4 "Docker     - Install" off)
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
 for choice in $choices
@@ -20,6 +21,9 @@ do
             ;;
         3)
             . ${MY_PATH}/lib/ddclient.install
+            ;;
+        4)
+            . ${MY_PATH}/lib/docker.install
             ;;
     esac
 done
